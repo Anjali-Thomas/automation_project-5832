06ac29121f666958f19512ed0fe40689d08a5411
Anjali Thomas <anjaliwrk@gmail.com>
2025-07-09 19:42:43 -0400

    final code updation

--- modules/common-5832/main.tf
+++ modules/common-5832/main.tf
@@ -20,3 +20,33 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+
+resource "azurerm_resource_group_template_deployment" "dummy1" {
+  name                = "dummy-deploy-1"
+  resource_group_name = var.rg_name
+  deployment_mode     = "Incremental"
+
+  template_content = jsonencode({
+    "$schema" = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
+    "contentVersion" = "1.0.0.0"
+    "resources" = []
+  })
+
+  parameters_content = "{}"
+}
+
+resource "azurerm_resource_group_template_deployment" "dummy2" {
+  name                = "dummy-deploy-2"
+  resource_group_name = var.rg_name
+  deployment_mode     = "Incremental"
+
+  template_content = jsonencode({
+    "$schema" = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
+    "contentVersion" = "1.0.0.0"
+    "resources" = []
+  })
+
+  parameters_content = "{}"
+}
+
--- modules/vmwindows-5832/provisioner.tf
+++ modules/vmwindows-5832/provisioner.tf
@@ -1,22 +1,22 @@
-resource "null_resource" "display_hostname" {
-  count = length(azurerm_windows_virtual_machine.vm)
+#resource "null_resource" "display_hostname" {
+ #count = length(azurerm_windows_virtual_machine.vm)
 
-  depends_on = [azurerm_windows_virtual_machine.vm]
+ #depends_on = [azurerm_windows_virtual_machine.vm]
 
-  connection {
-    type     = "winrm"
-    user     = "azureuser"
-    password = "Password1234!"
-    host     = azurerm_windows_virtual_machine.vm[count.index].public_ip_address
-    port     = 5985
-    https    = false
-    timeout  = "5m"
-  }
+ #connection {
+    #type     = "winrm"
+    #user     = "azureuser"
+    #password = "Password1234!"
+    #host     = azurerm_windows_virtual_machine.vm[count.index].public_ip_address
+    #port     = 5985
+    #https    = false
+    #timeout  = "5m"
+  #}
 
-  provisioner "remote-exec" {
-    inline = [
-      "hostname"
-    ]
-  }
-}
+  #provisioner "remote-exec" {
+    #inline = [
+      #"hostname"
+    #]
+  #}
+#}
 
