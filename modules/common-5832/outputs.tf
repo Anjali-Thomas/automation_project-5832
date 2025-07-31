761771acbacdb4e536d499c6558a48fc20f69b96
Anjali Thomas <anjaliwrk@gmail.com>
2025-07-31 19:18:21 -0400

    Update outputs and variables for log analytics workspace

--- modules/common-5832/outputs.tf
+++ modules/common-5832/outputs.tf
@@ -2,12 +2,22 @@ output "log_analytics_name" {
   value = azurerm_log_analytics_workspace.log.name
 }
 
+#output "log_analytics_workspace_id" {
+ # value = azurerm_log_analytics_workspace.log.id
+#}
+
+#output "log_analytics_primary_key" {
+ # value = azurerm_log_analytics_workspace.log.primary_shared_key
+ # sensitive = true
+#}
+
 output "log_analytics_workspace_id" {
-  value = azurerm_log_analytics_workspace.log.id
+  value = azurerm_log_analytics_workspace.log.workspace_id
 }
 
 output "log_analytics_primary_key" {
-  value = azurerm_log_analytics_workspace.log.primary_shared_key
+  value     = azurerm_log_analytics_workspace.log.primary_shared_key
+  sensitive = true
 }
 
 output "recovery_vault" {
--- modules/vmlinux-5832/variables.tf
+++ modules/vmlinux-5832/variables.tf
@@ -2,6 +2,10 @@ variable "rg_name" {}
 variable "location" {}
 variable "subnet_id" {}
 variable "storage_account" {}
-variable "log_analytics_workspace_id" {}
-variable "log_analytics_primary_key" {}
+variable "log_analytics_workspace_id" {
+	type = string
+		}
+variable "log_analytics_primary_key" {
+	type = string
+		}
 
